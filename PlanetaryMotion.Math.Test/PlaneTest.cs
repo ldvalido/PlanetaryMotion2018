cd2741e77b2c607aa77be42ddbf7280f4b5c8bd7
lvalido <lvalido@mailexternos.com.ar>
2016-11-01 15:50:17 -0300

    [PlanetaryMotion] Initial Structure

---
+++ PlanetaryMotion.Math.Test/PlaneTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Numerics;
+using PlanetaryMotion.Math.Extension;
+using Xunit;
+
+namespace PlanetaryMotion.Math.Test
+{
+    public class PlaneTest
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void TestAlignment()
+        {
+            var p1 = Complex.FromPolarCoordinates(1, 0);
+            var p2 = Complex.FromPolarCoordinates(1, System.Math.PI * 2);
+            var res = p1.IsAligned(p2);
+            Assert.True(res);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void TestNonAlignment()
+        {
+            var p1 = Complex.FromPolarCoordinates(1, 0);
+            var p2 = Complex.FromPolarCoordinates(1, System.Math.PI);
+            var res = p1.IsAligned(p2);
+            Assert.False(res);
+        }
+
+        [Fact]
+        public void TestInvalidPlane()
+        {
+            var p1 = Complex.FromPolarCoordinates(1, 0);
+            var p2 = Complex.FromPolarCoordinates(1, System.Math.PI * 2);
+            var p3 = Complex.FromPolarCoordinates(1, System.Math.PI * 4);
+            Assert.True(p1.IsAligned(p2));
+            Assert.True(p1.IsAligned(p3));
+            var plane = new Plane(p1,p2,p3);
+        }
+    }
+}
---
+++ PlanetaryMotion.Math/Plane.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+using PlanetaryMotion.Math.Extension;
+
+namespace PlanetaryMotion.Math
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Plane
+    {
+        #region C...tor
+        public Plane(Complex  p1,Complex p2,Complex p3)
+        {
+            if (!p1.IsAligned(p2) && !p1.IsAligned(p3))
+            {
+                P1 = p1;
+                P2 = p2;
+                P3 = p3;
+            }
+            else
+            {
+                throw new ArgumentException("The points are aligned and the plane cannot be built");
+            }
+        }
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// 
+        /// </summary>
+        public Complex P1 { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Complex P2 { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Complex P3 { get; set; }
+        #endregion
+    }
+}
