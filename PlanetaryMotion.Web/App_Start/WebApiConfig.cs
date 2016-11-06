1134f1489451ff08bfcdb189ebefc157320e6fb0
lvalido <lvalido@mailexternos.com.ar>
2016-11-06 15:51:29 -0300

    [PlanetaryMotion] Adequate WebSite

---
+++ PlanetaryMotion.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using Swashbuckle.Application;
+
+namespace PlanetaryMotion.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+            config.Routes.MapHttpRoute(
+            name: "swagger_root",
+            routeTemplate: "",
+            defaults: null,
+            constraints: null,
+            handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
+
+
+        }
+    }
+}
+
--- PlanetaryMotion.Web/Controllers/WeatherController.cs
+++ PlanetaryMotion.Web/Controllers/WeatherController.cs
@@ -2,19 +2,17 @@
 using PlanetaryMotion.Domain.Contract;
 using PlanetaryMotion.Model;
 
-namespace PlanetaryMotion.Web.Controller
+namespace PlanetaryMotion.Web.Controllers
 {
     [RoutePrefix("")]
     public class WeatherController : ApiController
     {
-        public IGalaxyService GalaxyService { get; set; } 
-
-        // GET api/<controller>
+        public IGalaxyService GalaxyService { get; set; }
         [Route("clima/{day:int}")]
-        public WeatherHistory GetByDay(int day)
+        public WeatherHistory GetWeather(int day)
         {
             var weatherCondition = GalaxyService.PredictWeather(day);
-            return new WeatherHistory {Day = day, Weather = weatherCondition};
+            return new WeatherHistory { Day = day, Weather = weatherCondition };
         }
     }
 }
