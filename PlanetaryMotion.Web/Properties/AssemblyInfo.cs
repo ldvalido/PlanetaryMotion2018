0cd5f93409350f5ac2cb5c68944a905540b5be73
lvalido <lvalido@mailexternos.com.ar>
2016-11-06 15:51:42 -0300

    [PlanetaryMotion] Adequate WebSite

--- PlanetaryMotion.Web/Properties/AssemblyInfo.cs
+++ PlanetaryMotion.Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("aac90879-a638-4de1-a117-bea1a6571ea0")]
+[assembly: Guid("4c3927c7-840b-47d1-b17a-6e4413b1a056")]
 
 // Version information for an assembly consists of the following four values:
 //
