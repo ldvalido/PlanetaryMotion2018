c1098f2bcb5f8907c5a246e338a6f5a2bfee702d
lvalido <lvalido@mailexternos.com.ar>
2016-11-06 11:19:47 -0300

    [PlanetaryMotion] Add Domain Project

---
+++ PlanetaryMotion.Model/Planet.cs
@@ -0,0 +1,30 @@
+﻿namespace PlanetaryMotion.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Planet
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the angle.
+        /// </summary>
+        /// <value>
+        /// The angle.
+        /// </value>
+        public double Angle { get; set; }
+        /// <summary>
+        /// Gets or sets the radious.
+        /// </summary>
+        /// <value>
+        /// The radious.
+        /// </value>
+        public double Radious { get; set; }
+    }
+}
---
+++ PlanetaryMotion.Web/Controller/WeatherController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace PlanetaryMotion.Web.Controller
+{
+    [RoutePrefix("")]
+    public class WeatherController : ApiController
+    {
+        // GET api/<controller>
+        [Route("clima")]
+        public IEnumerable<string> GetByDay()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
